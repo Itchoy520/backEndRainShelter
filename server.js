cdf49c9a5bce7e5992fc7f373b8295075370ec41
21-2918-830 Christian Paul Figuracion <christianpaul.figuracion@cit.edu>
2024-12-01 17:53:31 +0800

    Toggle

--- server.js
+++ server.js
@@ -14,6 +14,12 @@ app.get('/getvalue', (req, res) => {
     res.json({value: 1})
 });
 
+// Endpoint to toggle the LED State
+app.post('/toggle', (req, res) => {
+    ledState = ledState === 0 ? 1 : 0; // Toggle LED state
+    res.json({ success: true, value: ledState });
+})
+
 // Start the server
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
